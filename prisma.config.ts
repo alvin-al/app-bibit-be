eb412dc1d8753d200d9f2e9179f74aea1e745e68
Alvin Al <alvvxal@gmail.com>
2025-11-03 07:50:37 +0700

    Add all files

---
+++ prisma.config.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import * as path from "path";
+import { defineConfig, env } from "prisma/config";
+
+type Env = {
+  DATABASE_URL: string;
+};
+
+export default defineConfig({
+  engine: "classic",
+  datasource: {
+    url: env<Env>("DATABASE_URL"),
+  },
+  schema: path.join("prisma", "schema.prisma"),
+});
