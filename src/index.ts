b8c7048d9aec475e0ed746e752a9f4ecd5082673
alvinxal <alvvxal@gmail.com>
2025-11-04 16:10:56 +0700

    change import auth routes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import type { Request, Response } from "express";
-import authRoutes from "./api/routes/auth.routes";
+import authRoutes from "./api/routes/auth.routes.js";
 import dotenv from "dotenv";
 
 dotenv.config();
