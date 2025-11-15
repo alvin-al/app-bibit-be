1c7c293fc529f6f7a890d9b63db0fb2a4fd6bfbf
Alvin Al <alvvxal@gmail.com>
2025-11-15 14:00:33 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import type { Request, Response } from "express";
 import authRoutes from "./api/routes/auth.routes.js";
 import productRoutes from "./api/routes/product.routes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello backend");
