33c552fbcae9667d457fa84bafebb2c0a2ae3c9e
alvinxal <alvvxal@gmail.com>
2025-11-18 15:22:50 +0700

    feat: add image product

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import authRoutes from "./api/routes/auth.routes.js";
 import productRoutes from "./api/routes/product.routes.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import multer from "multer";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -16,9 +18,36 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello backend");
 });
 
+//upload file
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/uploads");
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage: storage });
+
 //Filtering route
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
+
+//upload route
+app.post("/api/upload", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "Tidak ada file yang diupload" });
+  }
+
+  const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+
+  res.json({ message: "Upload berhasil", url: fileUrl });
+});
+app.use("/uploads", express.static(path.join(process.cwd(), "public/uploads")));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
