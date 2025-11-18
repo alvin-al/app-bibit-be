afc007867066ed9b2d35536b17736c0fc91fdd61
alvinxal <alvvxal@gmail.com>
2025-11-18 21:37:51 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"], 
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello backend");
